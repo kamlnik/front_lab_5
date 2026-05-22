cd3577c30f8e01875b67dd04e5fe513bc3730714
kamlnik <kamlovskiynik@gmail.com>
2026-05-23 02:15:24 +0300

    Add files via upload

---
+++ Vue_Introduction_LAB5_2/my-clicker-app/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="clickers-grid">
+      <Clicker v-for="index in 100":key="index":amount="(index % 3) + 1" @button-clicked="() => showAlert(index)">
+        <template #default>
+          <button class="custom-btn"  :style="{ backgroundColor: getColor(index) }">
+            нажмите (+{{ (index % 3) + 1 }})
+          </button>
+        </template>
+      </Clicker>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import Clicker from './components/Clicker.vue';
+
+function showAlert(clickerNumber) {
+  alert(`Кликер ${clickerNumber} был нажат`);
+}
+
+const getColor = (i) => {
+  const colors = ['#ff9800', '#f44336', '#2196f3'];
+  return colors[i % colors.length];
+};
+</script>
+
+<style scoped>
+.clickers-grid {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+}
+.custom-btn {
+  border: none;
+  color: rgb(255, 255, 255);
+  padding: 6px 12px;
+  border-radius: 4px;
+  font-size: 12px;
+  cursor: pointer;
+  font-weight: bold;
+}
+</style>
---
+++ Vue_Introduction_LAB5_2/my-clicker-app/src/components/Clicker.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="clicker">
+    <span class="counter">{{ count }}</span>
+    <div class="click-area" @click="increment">
+      <slot>
+        <button class="default-btn">+{{ amount }}</button>
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// пропс amount (п.6)
+const props = defineProps({
+  amount: {
+    type: Number,
+    default: 1
+  }
+});
+
+const emit = defineEmits(['button-clicked']);
+
+const count = ref(0);
+
+const increment = () => {
+  count.value += props.amount;
+  emit('button-clicked');
+};
+
+
+
+</script>
